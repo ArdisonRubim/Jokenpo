4f2acfbdaed4fd8569d67a21c70f7d6caaa23204
Ardison Rubim <ardison.rubim@gmail.com>
2021-04-07 18:36:07 -0300

    Adcionado imagem

--- lib/Logica.dart
+++ lib/Logica.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class Logica extends StatefulWidget {
   @override
@@ -10,21 +11,32 @@ class _LogicaState extends State<Logica> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Joken Po"),
+        title: Text("Joken Po"), // Titulo
+        backgroundColor: Colors.orangeAccent,
       ),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+        crossAxisAlignment: CrossAxisAlignment.stretch, // Centraliza o conteudo
         children: <Widget>[
           Padding(
             padding: EdgeInsets.only(top: 40, bottom: 15),
             child: Text(
               "Escolha do app",
               textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold   // Cor do conteudo
+              ),
             ),
-          )
+          ),
+          Image.asset("images/padrao.png"),
         ],
       ),
-      bottomNavigationBar: Text("Rodapé"),
+      bottomNavigationBar: BottomAppBar( // Roda pe do app
+        color: Colors.orangeAccent,
+        child: Padding(
+          padding: EdgeInsets.all(20), // Espacamento em todos os lados
+        ),
+      ),
     );
   }
 }
